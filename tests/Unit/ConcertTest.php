20bcf3e7aeb3a9b983655bfe83b689ba18b88762
Brett Hartman <brett.hartman@mac.com>
2018-12-03 06:05:58 -0800

    Implement passing unit tests for getting formatted times, as well as prices in dollars

--- tests/Unit/ConcertTest.php
+++ tests/Unit/ConcertTest.php
@@ -4,22 +4,37 @@ namespace Tests\Unit;
 
 use App\Concert;
 use Carbon\Carbon;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
 class ConcertTest extends TestCase
 {
-	use DatabaseMigrations;
-
 	/** @test */
 	public function can_get_formatted_date(): void
 	{
-		$concert = factory(Concert::class)->create([
+		$concert = factory(Concert::class)->make([
 			'date' => Carbon::parse('2016-12-01 8:00pm')
 		]);
 
-		$date = $concert->formatted_date;
+		static::assertEquals('December 1, 2016', $concert->formatted_date);
+	}
 
-		static::assertEquals('December 1, 2016', $date);
+	/** @test */
+	public function can_get_formatted_start_time(): void
+	{
+		$concert = factory(Concert::class)->make([
+			'date' => Carbon::parse('2016-12-01 17:00:00')
+		]);
+
+		static::assertEquals('5:00pm', $concert->formatted_start_time);
+	}
+
+	/** @test */
+	public function can_get_ticket_price_in_dollars(): void
+	{
+		$concert = factory(Concert::class)->make([
+			'ticket_price' => 6750
+		]);
+
+		static::assertEquals('67.50', $concert->ticket_price_in_dollars);
 	}
 }
